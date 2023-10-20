f37aa50383b21f5a9694089bd28b205f12536986
SylvanCourtiol <sylvan.courtiol@protonmail.com>
2023-10-20 09:48:59 +0200

    debut api recipe content

--- chaudron-serveur/app.js
+++ chaudron-serveur/app.js
@@ -1,9 +1,35 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 
-app.get('/api', function (request, response) {
-    // TODO
+const prisma = new PrismaClient()
+
+app.get('/api/custom/recipes/:id/content', async function (request, response) {
+    const id = parseInt(request.params.id)
+    if (parseInt === NaN) {
+        // problème
+        response.statusCode = 400
+        response.send('Bad request')
+    }
+    const recipe = await prisma.Recipe.findUnique({
+        where: {
+          id: id,
+        },
+    })
+    if (recipe === undefined) {
+        response.statusCode = 404
+        response.send('Not found')
+    }
+    if (request.headers['content-type'] == 'text/html') {
+
+    } else if (request.headers['content-type'] == 'text/markdown') {
+        response.headers['content-type'] = 'text/markdown'
+        response.send(recipe.markdown) //TODO
+    } else {
+        response.statusCode = 400
+        response.send('Bad request')
+    }
 })
 
 app.use(express.static('./public'))
