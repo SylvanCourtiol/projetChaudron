db9f91b752364f03ab21ad3976671dc2e4f11c7f
n7student <quentin.dakou@gail.com>
2023-10-20 08:43:20 +0200

    ajout header et footer

---
+++ chaudron-client/src/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <h1>Mangez bien les reuss</h1>
+                <h1>Tout responsabilité d'intoxication n'est pas la notre</h1>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ chaudron-client/src/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>Chaudron</h1>
+                <h1>Vos meilleures recettes chaudronnées</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
