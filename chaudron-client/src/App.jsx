a64d5b23a3c86a1ec8d77e46a09349a00f842932
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2023-11-01 18:13:24 +0100

    acceuil v1

--- chaudron-client/src/App.jsx
+++ chaudron-client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,34 +7,64 @@ import Header from './header.jsx'
 import Footer from './footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [recipes, setRecipes] = useState([]);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    // Récupération des recettes
+    fetch('http://localhost:3009/recipes')
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error('Erreur de récupération des recettes', error));
+
+    // Récupération des notes
+    fetch('http://localhost:3009/marks')
+      .then((response) => response.json())
+      .then((data) => setNotes(data))
+      .catch((error) => console.error('Erreur de récupération des notes', error));
+  }, []);
+
+  console.log(JSON.stringify(recipes));
+  console.log(JSON.stringify(notes));
 
   return (
-    <>
-      <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer/>
-    </>
+    <div className="home-container">
+    <Header />
+
+    <div className="content">
+      <ul>
+        {recipes.map((recipe) => (
+              <li key={recipe.id}>
+              <a href={`/recipe/${recipe.id}`} className="recipe-link">
+                {recipe.name}
+                <span className="average-rating">{calculateAverageRating(recipe, notes)}</span>
+              </a>
+            </li>
+            
+            ))}
+      </ul>
+    </div>
+
+    <Footer />
+  </div>
+    
   )
 }
 
+
+function calculateAverageRating(recipe, notes) {
+  const recipeMarks = notes.filter((mark) => mark.recipe_id === recipe.id);
+
+  if (recipeMarks.length === 0) {
+    return 'Pas encore noté';
+  }
+
+  // Calculer la moyenne des notes
+  const totalRating = recipeMarks.reduce((sum, mark) => sum + mark.mark, 0);
+  const averageRating = totalRating / recipeMarks.length;
+  const roundedAverage = averageRating.toFixed(1);
+
+  return `${roundedAverage}/5`;
+}
 export default App
--- chaudron-serveur/app.js
+++ chaudron-serveur/app.js
@@ -1,6 +1,39 @@
 import express from 'express'
 
+import cors from 'cors'; // Utilisez import
+
 const app = express()
+import { PrismaClient } from '@prisma/client';
+
+const port = 3009;
+
+app.use(cors());
+
+const prisma = new PrismaClient();
+
+app.get('/recipes', async (req, res) => {
+    try {
+      const recipes = await prisma.recipe.findMany();
+      console.log(JSON.stringify(recipes));
+      console.log("Successfull")
+      res.json(recipes);
+    } catch (error) {
+      console.error('Erreur de récupératio des recettes', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    }
+  });
+
+  app.get('/marks', async (req, res) => {
+    try {
+      const marks = await prisma.recipeMark.findMany();
+      console.log(JSON.stringify(marks));
+      console.log("Successfull")
+      res.json(marks);
+    } catch (error) {
+      console.error('Erreur de récupératio des notes', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    }
+  });
 
 app.get('/api', function (request, response) {
     // TODO
@@ -8,6 +41,6 @@ app.get('/api', function (request, response) {
 
 app.use(express.static('./public'))
 
-app.listen(3000, function () {
-    console.log("Server listening on port 3000")
-  })
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  });
