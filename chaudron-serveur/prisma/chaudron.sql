abd3d4012db7668e16ce7d65b1a0f07d2b4177ce
n7student <quentin.dakou@gail.com>
2023-11-28 16:01:25 +0100

    maj bd

--- chaudron-serveur/prisma/chaudron.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO recipes (id, markdown, name) 
-VALUES (1, '# Aligot\n L aligot est un plat fait avec des pommes de terre et du fromage.', 'Aligot'),
-(2, '# Baguette\n La baguette est un type de pain typiquement français.', 'Baguette');
