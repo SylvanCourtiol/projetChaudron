d85b80996e22f43f129a5a369e1f567f971d1731
n7student <quentin.dakou@gail.com>
2023-12-07 11:47:56 +0100

    correction bug login + logins bouttons

---
+++ chaudron-client/src/logButtons.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { connectedUser } from './login';
+
+function LogButtons() {
+    const navigate = useNavigate();
+
+    const handleLogoutClick = () => {
+        sessionStorage.clear();
+        navigate('/')
+    };
+
+    const handleLoginClick = () => {
+        // Redirige vers la page de connexion
+        navigate('/login');
+    };
+  
+      const handleRegisterClick = () => {
+        // Redirige vers la page d'inscription
+        navigate('/register');
+    };
+
+    const username = sessionStorage.getItem('username')
+
+    if (connectedUser()) {
+        return(
+            <div style={{ textAlign: 'center'}}>
+                <button onClick={handleLogoutClick} className="btn btn-ghost normal-case text-xl mr-4">
+                Se déconnecter
+                </button>
+                <p style={{display: 'inline-block', textAlign: 'left'}}>Bienvenue: {username}</p>
+            </div>
+        );
+    } else {
+        return(
+            <div>
+                <button onClick={handleLoginClick} className="btn btn-ghost normal-case text-xl mr-4">
+                Se connecter
+                </button>
+                <button onClick={handleRegisterClick} className="btn btn-ghost normal-case text-xl">
+                S'inscrire
+                </button>
+            </div>
+        )
+    }
+}
+
+export default LogButtons;
